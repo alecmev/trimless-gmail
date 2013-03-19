b9af8e3f4991ab29f7b1c3010ea99f712c2c1c64
Olegs Jeremejevs <olegs@jeremejevs.com>
2013-03-19 03:53:50 +0000

    a lot of refactoring, default color is hardcoded now, fixes #1, fixes #3, fixes #4, fixes #7, fixes #8

--- trimless-gmail.js
+++ trimless-gmail.js
@@ -5,30 +5,39 @@
 
 function untrim()
 {
-    $('.ajV, .ajU, .aH1').click().remove();
-    $('.im').css('cssText', 'color: ' + $('.hx').css('color') + ' !important');
-    
-    var firstLineBreak = $('.im > .gmail_extra > br:first-child:not(.leave-alone)');
-
-    if (firstLineBreak.length > 0)
-    {
-        firstLineBreak.next('br').addClass('leave-alone');
-        firstLineBreak.remove();
-    }
+    $('.ajT').parent().parent().remove();
+    $('.adL, .adL > div, .h5, .adM').css('display', 'block');
 }
 
 function untrimForSure()
 {
     untrim();
     window.setTimeout(untrim, 1000);
+    window.setTimeout(untrim, 2000);
 }
 
-function untrimNotSure(event)
+function initialize()
 {
-    if (!$(event.target).is('.ajV, .ajU, .aH1'))
-        untrimForSure();
+    untrimForSure();
+
+    var styleSheets = document.styleSheets;
+    var containers = ['.im', '.gmail_quote', '.h5'];
+
+    for (var i = 0; i < styleSheets.length; i++)
+    {
+        var rules = styleSheets[i].cssRules || styleSheets[i].rules;
+
+        for (var j = 0; j < rules.length; j++)
+        {
+            for (var k = 0; k < containers.length; k++)
+            {
+                if (rules[j].selectorText === containers[k])
+                    rules[j].style.color = '#222';
+            }
+        }
+    }
 }
 
+$(window).load(initialize);
 $(window).bind('hashchange', untrimForSure);
-$(document).click(untrimNotSure);
-untrimForSure();
+$(document).click(untrimForSure);
