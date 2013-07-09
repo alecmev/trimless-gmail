c81da89a5f40c934f525b9046dc745bc614b6892
Olegs Jeremejevs <olegs@jeremejevs.com>
2013-07-09 22:15:25 +0300

    fix #15, add google analytics, fix the extension not working until a click

---
+++ background.js
@@ -0,0 +1,53 @@
+if (!('trimless-enabled' in localStorage))
+    localStorage['trimless-enabled'] = true;
+
+function updateIcon(tabId, isEnabled)
+{
+    chrome.pageAction.setIcon({ tabId: tabId, path: {
+        '19': 'icon-action' + (isEnabled ? '' : '-gray') + '-19.png',
+        '38': 'icon-action' + (isEnabled ? '' : '-gray') + '-38.png'
+    }});
+    chrome.pageAction.setTitle({ tabId: tabId, 
+        title: (isEnabled ? 'Trimless is enabled' : 'Trimless is disabled')
+    });
+}
+
+chrome.pageAction.onClicked.addListener(function(tab)
+{
+    chrome.tabs.sendMessage(tab.id, false, function(isEnabled)
+    {
+        updateIcon(tab.id, isEnabled);
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
+{
+    chrome.tabs.sendMessage(tabId, true, function(data)
+    {
+        if (data != undefined && 'isEnabled' in data)
+        {
+            chrome.pageAction.show(tabId);
+            updateIcon(tabId, data.isEnabled);
+        }
+    });
+});
+
+chrome.runtime.onMessage.addListener(function(isEnabled, sender, sendResponse)
+{
+    window.setTimeout(function() {
+        updateIcon(sender.tab.id, isEnabled);
+    }, 100);
+    window.setTimeout(function() {
+        updateIcon(sender.tab.id, isEnabled);
+    }, 200);
+});
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40792111-3']);
+_gaq.push(['_trackPageview']);
+var ga = document.createElement('script');
+ga.type = 'text/javascript';
+ga.async = true;
+ga.src = 'https://ssl.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
--- trimless-gmail.js
+++ trimless-gmail.js
@@ -1,32 +1,86 @@
+var isEnabled;
+checkStorage(true);
+
+function checkStorage(isFirstTime)
+{
+    var oldValue = isEnabled;
+    isEnabled = JSON.parse(localStorage['trimless-enabled']);
+    if (!isFirstTime && oldValue != isEnabled)
+        chrome.runtime.sendMessage(isEnabled);
+}
+
 function untrim()
 {
-    $('.adL, .adM, .h5, .h4').css('display', 'block');
-    $('.ajR').not(':hidden').children('.aH1').click();
-    $('.adm, .ajU, .ajV').remove();
+    $('.adL, .adM, .h5, .h4').filter("[display!='block']")
+        .css('display', 'block').addClass('trimless-block');
+    $('.ajR').not(':hidden').hide().addClass('trimless-button1')
+        .children('.aH1').click().addClass('trimless-button2');
+    $('.adm, .ajU, .ajV').hide().addClass('trimless-button3');
+
+    style();
+}
+
+function ununtrim()
+{
+    $('.trimless-block').css('display', 'none');
+    $('.trimless-button1').show();
+    $('.trimless-button2').click();
+    $('.trimless-button3').show();
+
+    unstyle();
+}
+
+function style()
+{
+    $('.adL > .im, .gmail_quote, .h5, blockquote > div')
+        .addClass('trimless-padding trimless-color').find('*')
+        .addClass('trimless-color').removeClass('trimless-padding');
+}
+
+function unstyle()
+{
+    $('.trimless-padding, .trimless-color')
+        .removeClass('trimless-padding').removeClass('trimless-color');
 }
 
 function untrimForSure()
 {
-    untrim();
-    window.setTimeout(untrim, 1000);
-    window.setTimeout(untrim, 2000);
-    styleUntrimmed();
+    checkStorage(false);
+    if (isEnabled) {
+        untrim();
+        window.setTimeout(untrim, 1000);
+        window.setTimeout(untrim, 2000);
+    }
+    else
+        ununtrim();
 }
 
 function untrimOnClick(event)
 {
-    if (!$(event.target).is('.aH1'))
-        untrimForSure();
+    if (isEnabled) {
+        if (!$(event.target).is('.aH1'))
+            untrimForSure();
+    }
 }
 
-function styleUntrimmed()
+chrome.runtime.onMessage.addListener(function(isCheck, sender, sendResponse)
 {
-    $('.adL > .im, .gmail_quote, .h5, blockquote > div')
-        .addClass('trimless-padding trimless-color')
-        .find('*').addClass('trimless-color');
-}
+    if (isCheck)
+        sendResponse({ isEnabled: isEnabled });
+    else {
+        isEnabled = !isEnabled;
+        localStorage['trimless-enabled'] = isEnabled;
+        if (isEnabled)
+            untrim();
+        else
+            ununtrim();
+
+        sendResponse(isEnabled);
+    }
+});
 
 untrimForSure();
 $(window).bind('hashchange', untrimForSure);
+$(document).bind('webkitvisibilitychange', untrimForSure);
 $(document).click(untrimOnClick);
 $(window).load(untrimForSure);
