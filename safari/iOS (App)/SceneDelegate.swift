3cdaf745dd158936b7c82c1ea37f9d8208152f83
seidnerj <jonathan.seidner@gmail.com>
2023-03-28 16:27:25 +0300

    Added a Safari extension project
    This was done by running "xcrun safari-web-extension-converter ." in the project's directory with minor tweaks to the project file. Note that in order to use this on your machine the built extension would either need to signed or "allow unsigned extensions" to be checked in Safari's interface.

---
+++ safari/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created on 28/03/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
